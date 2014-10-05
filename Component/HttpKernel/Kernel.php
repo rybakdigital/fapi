6b4327373b9406d5ae16fad697ac93f16fe37218
krisrybak <kris.rybak@hotmail.co.uk>
2014-10-05 16:27:29 +0100

    Adds Kernel file. The hart of fapi system

---
+++ Component/HttpKernel/Kernel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Fapi\Component\HttpKernel;
+
+/**
+ * Fapi\Component\HttpKernel
+ *
+ * The Kernel is the heart of the Fapi system.
+ *
+ * @author  Kris Rybak <kris@krisrybak.com>
+ */
+abstract class Kernel
+{
+
+}
