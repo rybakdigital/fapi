0f54a89a30f52cfe90312ac489ac945032e65508
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-14 15:18:48 +0000

    WIP - Routing

--- Component/Routing/Router.php
+++ Component/Routing/Router.php
@@ -42,7 +42,7 @@ class Router implements RouterInterface
     public function getRouteCollection()
     {
         if (null === $this->collection) {
-            $this->collection = $this->loadRouteCollection($this->getResourse());
+            $this->collection = $this->loadRouteCollection();
         }
 
         return $this->collection;
@@ -57,13 +57,70 @@ class Router implements RouterInterface
         return $this->resource;
     }
 
-    public function loadResurce()
+    public function loadResurce($modulePath = NULL)
     {
-        
+        $include_paths  = explode(PATH_SEPARATOR, get_include_path());
+
+        foreach ($include_paths as $path) {
+            $modulePath = $this->resolveFilePathSlashes($modulePath);
+            if (file_exists("../app/config/" . $modulePath . "routing.php")) {
+               //var_dump('ok');
+            }
+            //var_dump( "../app/config/" . $modulePath . "routing.php");
+        }
     }
 
-    public function loadRouteCollection($resource)
+    private function resolveFilePathSlashes($path)
     {
+        if (false !== $lastPos = strrpos($path, '/')) {
+            if (($lastPos + 1) != strlen($path)) {
+                $path = $path.'/';
+            }
+        }
+        if (0 == $firstPos = strpos($path, '/')) {
+            $path = substr($path, 1);
+        }
+        
+        return $path;
+    }
 
+    public function loadRouteCollection($resource = null)
+    {
+        $routes = array();
+
+        $basePath = '../';
+
+        if ($resource == null) {
+            $resource = "app/config/routing.yml";
+        }
+
+        if (!(substr($resource, -3, 3) == 'yml')) {
+            $resource = $resource . '/routing.yml';
+        }
+
+        if (file_exists($basePath . $resource)) {
+            $file = file_get_contents($basePath . $resource);
+
+            $array  = Yaml::parse($file);
+
+            if (is_array($array)) {
+                if (isset($array['imports'])) {
+                    foreach ($array['imports'] as $import) {
+                        if (isset($import['resource'])) {
+                            $this->loadRouteCollection($import['resource']);
+                        }
+                    }
+                }
+
+                if (isset($array['routes'])) {
+                    foreach ($array['routes'] as $name => $route) {
+                        $routes[$name] = $route;
+                    }
+                }
+            }
+        }
+
+
+        var_dump($routes);
     }
 }
