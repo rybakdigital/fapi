a9baa0738c96c71c90cf2a56a81d4536c462535d
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-15 08:54:06 +0000

    Adds file for a matcher

---
+++ Component/Routing/Mathcer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fapi\Component\Routing;
+
+/**
+ * Fapi\Component\Routing\Matcher
+ *
+ * Matches Route for the request.
+ *
+ * @author  Kris Rybak <kris@krisrybak.com>
+ */
+class Matcher
+{
+}
