de815687451c1e5a08e8d728e5ff27b5f0a29eb3
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-15 09:16:51 +0000

    Adds MatcherInterface

---
+++ Component/Routing/Matcher.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Fapi\Component\Routing;
+
+use Fapi\Component\Routing\Matcher\MatcherInterface;
+
+/**
+ * Fapi\Component\Routing\Matcher
+ *
+ * Matches Route for the request.
+ *
+ * @author  Kris Rybak <kris@krisrybak.com>
+ */
+class Matcher implements MatcherInterface
+{
+}
---
+++ Component/Routing/Matcher/MatcherInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Fapi\Component\Routing\Matcher;
+
+/**
+ * Fapi\Component\Routing\Matcher\MatcherInterface
+ *
+ * @author  Kris Rybak <kris@krisrybak.com>
+ */
+interface MatcherInterface
+{
+}
