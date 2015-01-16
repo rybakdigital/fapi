1a41d9b89f26b3416aaa52c499c62d4e03340910
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-16 11:13:05 +0000

    Matches Route by getting candidates

--- Component/Routing/Matcher.php
+++ Component/Routing/Matcher.php
@@ -20,19 +20,80 @@ class Matcher implements MatcherInterface
      */
     protected $request;
 
-
     /**
      * @var RouteCollection
      */
     protected $collection;
+
+    private static $regexOperands = array(
+        'int' => '\d+',
+        'str' => '\w+',
+    );
 
     /**
      * Matches current Request to Route
      */
     public function match(RouteCollection $collection, Request $request)
     {
-        foreach ($collection as $route) {
-            var_dump($route);
+        // Save collection and Request
+        $this->collection   = $collection;
+        $this->request      = $request;
+
+        // Get candidates and ask Voter to decide which candidate mathes best
+        $this->candidates   = $this->getCandidates();
+    }
+
+    public function getCandidates()
+    {
+        $candidates = array();
+
+        foreach ($this->collection->all() as $name => $route) {
+            $specs = array();
+
+            preg_match_all('/\{\w+\}/', $route->getPath(), $matches);
+
+            if (isset($matches[0])) {
+                $specs = $matches[0];
+            }
+
+            foreach ($specs as $spec) {
+                $param          = substr($spec, 1, -1);
+                $regexSpec      = '\\'.$spec.'\\';
+                $requirements   = $route->getRequirements();
+
+                if (isset($requirements[$param])) {
+                    $route->setRegex(
+
+                        str_replace(
+                            $spec,
+                            $this->getRegexOperand($requirements[$param]),
+                            $route->getRegex()
+                        )
+                    );
+                }
+            }
+
+            // Build regeular expression to match routes
+            $route->setRegex('^'.'/'.ltrim(trim($route->getRegex()), '/').'/?$');
+            $route->setRegex('/'.str_replace('/', '\/', $route->getRegex()).'/');
+
+            if (preg_match($route->getRegex(), $this->request->getPathInfo())) {
+                // Check the rquest method matches route methods
+                if (in_array($this->request->getMethod(), $route->getMethods())) {
+
+                    // We have a match
+                    $candidates[] = $route;
+                }
+            }
+        }
+
+        return $candidates;
+    }
+
+    private function getRegexOperand($operand)
+    {
+        if (array_key_exists($operand, self::$regexOperands)) {
+            return self::$regexOperands[$operand];
         }
     }
 }
