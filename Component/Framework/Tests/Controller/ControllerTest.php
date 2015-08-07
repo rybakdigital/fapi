e4b471fd64ac77114de7702dc66de0c94a683f25
krisrybak <kris.rybak@hotmail.co.uk>
2015-08-07 09:38:06 +0100

    Adds test for setValidator

--- Component/Framework/Tests/Controller/ControllerTest.php
+++ Component/Framework/Tests/Controller/ControllerTest.php
@@ -4,6 +4,7 @@ namespace Fapi\Component\Framework\Tests\Controller;
 
 use \PHPUnit_Framework_TestCase as TestCase;
 use Fapi\Component\Framework\Controller\Controller;
+use Ucc\Data\Validator\Validator;
 
 class ContrllerTest extends TestCase
 {
@@ -11,5 +12,12 @@ class ContrllerTest extends TestCase
     {
         $controller = new Controller;
         $this->assertInstanceOf('Ucc\Data\Validator\ValidatorInterface', $controller->getValidator());
+    }
+
+    public function testSetValidator()
+    {
+        $controller     = new Controller;
+        $validator      = new Validator;
+        $this->assertInstanceOf(get_class($controller), $controller->setValidator($validator));
     }
 }
