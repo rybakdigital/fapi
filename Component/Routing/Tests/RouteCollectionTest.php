7fc7453f863cc83466d290729d445953da1be307
krisrybak <kris.rybak@hotmail.co.uk>
2015-08-18 11:04:23 +0100

    Adds file for RouteCollection test

---
+++ Component/Routing/Tests/RouteCollectionTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Fapi\Component\Routing\Tests\Route;
+
+use \PHPUnit_Framework_TestCase as TestCase;
+use Fapi\Component\Routing\RouteCollection;
+
+class RouteCollectionTest extends TestCase
+{
+}
