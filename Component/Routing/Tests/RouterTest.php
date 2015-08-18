f1592bf6e7b35216415cbf265d0eb429b8cdb3df
krisrybak <kris.rybak@hotmail.co.uk>
2015-08-18 15:39:14 +0100

    Adds test for parseRoute

--- Component/Routing/Tests/RouterTest.php
+++ Component/Routing/Tests/RouterTest.php
@@ -3,7 +3,30 @@
 namespace Fapi\Component\Routing\Tests;
 
 use \PHPUnit_Framework_TestCase as TestCase;
+use Fapi\Component\Routing\Router;
+use Symfony\Component\HttpFoundation\Request;
 
 class RouterTest extends TestCase
 {
+    public function routeProviderForParser()
+    {
+        return array(
+            array(array(
+                'path'          => '',
+                'controller'    => 'Home',
+                'methods'       => array('POST', 'PUT'),
+                'calls'         => 'index',
+            )),
+        );
+    }
+
+    /**
+     * @dataProvider routeProviderForParser
+     */
+    public function testParseRoute($routeSpec)
+    {
+        $router = new Router(new Request);
+
+        $this->assertInstanceOf('Fapi\Component\Routing\Route\Route', $router->parseRoute($routeSpec));
+    }
 }
